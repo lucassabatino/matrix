cc3218b73d6d2b45ea14c7b1a03c3762f2577de1
lucassabatino <62308698+lucassabatino@users.noreply.github.com>
2020-03-23 08:02:27 -0300

    Update app.config.js

--- backend/app/app.config.js
+++ backend/app/app.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ENVIRONMENT: process.env.NODE_ENV,
   GOOGLE_CREDENTIAL:
     process.env.GOOGLE_CREDENTIAL
-    || "924991972177-9uuj35peccqsnk0pdnupok5qp86lpo1c.apps.googleusercontent.com",
+    || "529023224442-uc59r7lmmfbghgsio06sbkv6rnnaibae.apps.googleusercontent.com",
   ENFORCE_SSL: process.env.ENFORCE_SSL || "false",
   CUSTOM_STYLE: process.env.CUSTOM_STYLE || "",
   HOST: "0.0.0.0",
